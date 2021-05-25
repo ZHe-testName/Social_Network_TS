fa7a9166a838de698a1eca76f8c0e443c973b93a
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-05-25 20:24:56 +0300

    Added ref and its type

--- src/components/profile/my-posts/new-post/NewPost.tsx
+++ src/components/profile/my-posts/new-post/NewPost.tsx
@@ -1,13 +1,25 @@
+import React from 'react';
 import classes from './newpost.module.css';
 
 function NewPost() {
+    const newPostElement = React.createRef<HTMLTextAreaElement>();
+
+    const addNewPostHandler = () => {
+        alert(newPostElement.current?.value);
+    };
+
     return (
         <div className={classes.new_post}>
-            <textarea  cols={90} rows={5} placeholder="Typing your post here...">
+            <textarea  
+                    ref={newPostElement}
+                    cols={90} 
+                    rows={5} 
+                    placeholder="Typing your post here...">
                 
             </textarea>
             
-            <button className="send">Send</button>
+            <button className="send"
+                    onClick={addNewPostHandler}>Add post</button>
         </div>
     );
 };
