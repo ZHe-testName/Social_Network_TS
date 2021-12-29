19711fc790ccbb5c8e2d8599a1b2fc244cd8f7e1
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-12-29 20:56:04 +0200

    Added funtional profile status

---
+++ src/components/profile/profile-status/ProfileStatusFunctional.tsx
@@ -0,0 +1,74 @@
+import React, { KeyboardEvent, useState }  from "react";
+import classes from "./profile_status.module.css";
+
+type StatusPropsType = {
+    titleStatus: string,
+    updateStatus: (status: string) => void,
+};
+
+type LocalStateType = {
+    editMode: boolean,
+    status: string,
+    updateStatus: (status: string) => void,
+};
+
+const ProfileStatusFunctional: React.FC<StatusPropsType> = ({titleStatus, updateStatus}) => {
+    const [isEditMode, setEditMode] = useState(false);
+    const [status, changeStatus] = useState(titleStatus);
+
+    // onEditMode = () => {
+    //     this.setState({
+    //         editMode: true,
+    //     });
+    // }
+
+    const offEditMode = () => {
+        updateStatus(status);
+
+        setEditMode(false);
+    }
+
+    const offEditModeWithEnter = (e: KeyboardEvent) => {
+        if (e.code === 'Enter') {
+            setEditMode(false);
+        };
+    }
+
+    // componentDidUpdate(prevProps: StatusPropsType, prevState: LocalStateType) {
+    //     console.log('yo');
+    //     if (prevProps.titleStatus !== this.props.titleStatus) {
+    //         this.setState({
+    //             status: this.props.titleStatus,
+    //         });
+    //     };
+    // }
+
+    return (
+        <div className={classes.status}>
+            {
+                isEditMode 
+
+                ?
+                <div>
+                    <input 
+                        defaultValue={status}
+                        autoFocus
+                        onBlur={offEditMode}
+                        onKeyPress={e => offEditModeWithEnter(e)}
+                        onChange={e => changeStatus(e.target.value)}>
+                    </input>
+                </div>
+
+                :
+                <div>
+                    <span
+                        onDoubleClick={() => setEditMode(true)}>
+                        {status || 'no status'}
+                    </span>
+                </div>
+            }
+        </div>
+    )
+};
+
+export default ProfileStatusFunctional;
